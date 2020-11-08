e445ed2f87b61f24143532cf3ac5083725195847
Arkadiusz Bryczek <arkadiusz.bryczek@o2.pl>
2020-11-08 17:04:19 +0100

    Final Version

--- src/main/java/com/dockerapp/citydates/lab4/controller/DatesController.java
+++ src/main/java/com/dockerapp/citydates/lab4/controller/DatesController.java
@@ -2,6 +2,8 @@ package com.dockerapp.citydates.lab4.controller;
 
 import com.dockerapp.citydates.lab4.model.CityDates;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 
-@RestController
+@Controller
 public class DatesController {
     @GetMapping("/getDates")
     public ResponseEntity<CityDates> getDatesM(){
@@ -19,5 +21,19 @@ public class DatesController {
                 LocalDateTime.now(ZoneId.of("Australia/Sydney"))
         );
         return ResponseEntity.ok(cityDates);
+    }
+
+    @GetMapping("/getDatesS")
+    public String getDatesS(Model model){
+        CityDates cityDates = new CityDates(
+                LocalDateTime.now(ZoneId.of("Europe/Warsaw")),
+                LocalDateTime.now(ZoneId.of("America/New_York")),
+                LocalDateTime.now(ZoneId.of("Australia/Sydney"))
+        );
+
+        model.addAttribute("lublinDate", cityDates.getLublin());
+        model.addAttribute("newYorkDate", cityDates.getNew_york());
+        model.addAttribute("sydneyDate", cityDates.getSydney());
+        return "dates";
     }
 }
